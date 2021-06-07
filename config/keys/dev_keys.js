fb0221438787b0671cae56d9872617bd3f1f0a77
Billuboy <billuboy1718@gmail.com>
2021-06-07 11:33:12 +0530

    Keys Secured

--- config/keys/dev_keys.js
+++ config/keys/dev_keys.js
@@ -1,7 +1,2 @@
 module.exports = {
-  secretKey: 'secretKey',
-  dbURI:
-    'mongodb+srv://Billuboy:057ad5c2@@devdo.zxaj0.mongodb.net/devdo?retryWrites=true&w=majority',
-  gmailId: 'manujkaushik171801@gmail.com',
-  gmailPass: process.env.GMAIL_PASS,
 };

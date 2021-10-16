e3bf983abda364bf080a594529f8a06ac29c03a6
Billuboy <72341331+Billuboy@users.noreply.github.com>
2021-10-16 06:29:07 +0530

    Delete middleware directory

--- middleware/error.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (err, req, res, next) {
-  throw new Error('INTERNAL_SERVER_ERROR', {
-    errors: {
-      server: 'Some Internal Server Error Occured',
-    },
-  });
-};
